ea92d3d9382abd909f12d476c8d375875515c781
hexaalif <shadullahsad9@gmail.com>
2024-01-27 00:58:25 +0600

    postgre on render added

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -84,15 +85,22 @@ WSGI_APPLICATION = 'mamar_bank.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgres://fokira_bank_user:EHheAQ8ksfw1UWazWP61Va9UkwZr0B2M@dpg-cmq00m7qd2ns738u3o9g-a.oregon-postgres.render.com/fokira_bank',
+    )
 }
 
 
